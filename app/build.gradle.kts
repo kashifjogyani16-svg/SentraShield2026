f97701a6e7fc2e93a21a00f5180cc7839ed57327
kashifjogyani16-svg <kashifjogyani16@gmail.com>
2026-09-26 19:13:15 +0500

    Fix: use Gradle build, remove package attr from manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
 android {
@@ -55,6 +56,7 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
     // Retrofit + Gson
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "SentraShield"
 include(":app")
