dc4c5bce5a4bc6e2fb00b05712c8d83e66a81a61
kashifjogyani16-svg <kashifjogyani16@gmail.com>
2026-09-26 10:52:03 +0500

    Fix PermissionUtil canDrawOverlay and requestOverlay

--- app/src/main/java/com/sentra/shield/util/PermissionUtil.kt
+++ app/src/main/java/com/sentra/shield/util/PermissionUtil.kt
@@ -6,20 +6,16 @@ import android.net.Uri
 import android.provider.Settings
 
 object PermissionUtil {
+    fun canDrawOverlay(context: Context): Boolean {
+        return Settings.canDrawOverlays(context)
+    }
 
-    fun hasOverlayPermission(context: Context): Boolean =
-        Settings.canDrawOverlays(context)
-
-    fun overlaySettingsIntent(context: Context): Intent =
-        Intent(
+    fun requestOverlay(context: Context) {
+        val intent = Intent(
             Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
             Uri.parse("package:${context.packageName}")
         )
-
-    fun usageAccessSettingsIntent(): Intent =
-        Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
-
-    fun appNotificationSettingsIntent(context: Context): Intent =
-        Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-            .putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
+    }
 }
